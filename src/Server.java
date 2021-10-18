1782601158d8db144ab16dbefafed622a0d78ca2
Nino <nino.delien@gmail.com>
2021-10-18 09:49:28 +0200

    accept player ok

--- src/Server.java
+++ src/Server.java
@@ -6,26 +6,30 @@ import java.io.*;
 public class Server implements Runnable {
 	
 	private ArrayList<Thread> tabThread;
-	private ArrayList<Socket> socket;
+	private ArrayList<Socket> tabSocket;
 	private int nbjoueur;
 	private ServerSocket gestSock;
 	
 	public Server() {
 		System.out.println("in Server");
+		tabSocket = new ArrayList<Socket>();
+		tabThread = new ArrayList<Thread>();
 		try {
 			nbjoueur=0;
 			gestSock = new ServerSocket(10000);
 			while(true) {
 				System.out.println("before accept"+nbjoueur);;
 				Socket gs = gestSock.accept();
-				System.out.println(gs);
-				socket.add(gs);
+//				System.out.println(gs);
+//				System.out.println(tabSocket);
+				tabSocket.add(gs);
+//				System.out.println("test");
 				tabThread.add(new Thread(this));
-				System.out.println("nbjoueur");
+				System.out.println("before accept"+nbjoueur);
 				tabThread.get(nbjoueur).start();
 				
 				nbjoueur ++; 
-				System.out.println("after accept");
+//				System.out.println("after accept");
 		}
 		//gestSock.close();
 		} catch (IOException e) {e.printStackTrace( );} 
@@ -42,8 +46,8 @@ public class Server implements Runnable {
 	public void run() {
 		System.out.println("run");
 		try {
-			DataInputStream entree = new DataInputStream(socket.get(nbjoueur).getInputStream());
-			DataOutputStream sortie = new DataOutputStream(socket.get(nbjoueur).getOutputStream());
+			DataInputStream entree = new DataInputStream(tabSocket.get(nbjoueur).getInputStream());
+			DataOutputStream sortie = new DataOutputStream(tabSocket.get(nbjoueur).getOutputStream());
 			/*String nomJoueur = entree.readUTF();*/
 			/*System.out.println(nomJoueur+"connected");*/
 		} catch (IOException e) {
